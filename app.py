f9c956bd474444210609dac3f260d81e17f37933
Sofia Shanshina <s.shanshina@gmail.com>
2022-07-24 20:57:20 +0700

    Add advertisements

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import uuid
 import datetime
 import pydantic
 from typing import Union
@@ -7,7 +8,8 @@ from flask_bcrypt import Bcrypt
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, create_engine, func
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.dialects.postgresql import UUID
 
 app = Flask('app')
 bcrypt = Bcrypt(app)
@@ -55,10 +57,45 @@ class Advertisement(Base):
     title = Column(String, nullable=False)
     description = Column(String(500), nullable=True)
     created_at = Column(DateTime, server_default=func.now())
-    owner_id = Column(Integer, ForeignKey(User.id))
+    owner_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    @classmethod
+    def creation(cls, session: Session, title: str, description: str, owner_id: int):
+        new_advertisement = Advertisement(
+            title=title,
+            description=description,
+            owner_id=owner_id,
+        )
+        session.add(new_advertisement)
+        return new_advertisement
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'created_at': self.created_at,
+            'owner_id': self.owner_id,
+        }
+
+
+class Token(Base):
+
+    __tablename__ = 'tokens'
+    id = Column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
+    created_at = Column(DateTime, server_default=func.now())
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship(User, lazy='joined')
 
 
 Base.metadata.create_all(engine)
+
+
+def get_object_or_404(session, model, *criterion):
+    object = session.query(model).filter(*criterion).first()
+    if object is None:
+        raise HTTPError(404, 'object not found')
+    return object
 
 
 class HTTPError(Exception):
@@ -77,7 +114,29 @@ def handle_errors(error):
     return response
 
 
-class RegisterUser(pydantic.BaseModel):
+def check_token(session):
+    token = (
+        session.query(Token)
+        .join(User)
+        .filter(
+            User.user_name == request.headers.get('user_name'),
+            Token.id == request.headers.get('token'),
+        )
+        .first()
+    )
+    if token is None:
+        raise HTTPError(401, 'invalid token or user name')
+    return token
+
+
+def validation(unvalidated_data: dict, validation_model):
+    try:
+        return validation_model(**unvalidated_data).dict()
+    except pydantic.error_wrappers.ValidationError as error:
+        raise HTTPError(400, error.errors())
+
+
+class CreateUser(pydantic.BaseModel):
     user_name: str
     email: str
     password: str
@@ -89,20 +148,6 @@ class RegisterUser(pydantic.BaseModel):
         return value
 
 
-def validation(unvalidated_data: dict, validation_model):
-    try:
-        return validation_model(**unvalidated_data).dict()
-    except pydantic.error_wrappers.ValidationError as error:
-        raise HTTPError(400, error.errors())
-
-
-def get_object_or_404(session, model, *criterion):
-    user = session.query(model).filter(*criterion).first()
-    if user is None:
-        raise HTTPError(404, 'user not found')
-    return user
-
-
 class UserView(MethodView):
     def get(self, user_id):
         with Session() as session:
@@ -111,42 +156,87 @@ class UserView(MethodView):
 
     def post(self):
         with Session() as session:
-            new_user = User.registration(session, **validation(request.json, RegisterUser))
+            new_user = User.registration(session, **validation(request.json, CreateUser))
             try:
                 session.commit()
                 return jsonify(new_user.to_dict())
             except IntegrityError:
                 raise HTTPError(400, 'user already exists')
 
-# class AdvertisementView(MethodView):
-#     def get(self, advertisement_id: int):
-#         with Session() as session:
-#             pass
-#
-#     def post(self):
-#         advertisement_data = request.json
-#         with Session() as session:
-#             new_advertisement = Advertisement(
-#                 title=advertisement_data['title'],
-#                 description=advertisement_data['description'],
-#                 owner_id=User.id,
-#             )
-#             session.add(new_advertisement)
-#             session.commit()
+
+class CreateAdvertisement(pydantic.BaseModel):
+    title: str
+    description: str
+
+
+class AdvertisementView(MethodView):
+    def get(self, advertisement_id: int):
+        with Session() as session:
+            advertisement = get_object_or_404(session, Advertisement, Advertisement.id == advertisement_id)
+            return jsonify(advertisement.to_dict())
+
+    def post(self):
+        advertisement_data_validated = validation(request.json, CreateAdvertisement)
+        with Session() as session:
+            token = check_token(session)
+            if token:
+                advertisement_owner = get_object_or_404(session, User, User.user_name == request.headers.get('user_name'))
+                advertisement_data_validated['owner_id'] = advertisement_owner.id
+                new_advertisement = Advertisement.creation(session, **advertisement_data_validated)
+                session.commit()
+                return jsonify(new_advertisement.to_dict())
+
+    def delete(self, advertisement_id: int):
+        with Session() as session:
+            advertisement = get_object_or_404(session, Advertisement, Advertisement.id == advertisement_id)
+            token = check_token(session)
+            if token.user.id != advertisement.owner_id:
+                raise HTTPError(403, 'auth error')
+            session.query(Advertisement).filter(Advertisement.id == advertisement_id).delete()
+            session.commit()
+            return jsonify({
+                'message': 'your advertisement has been successfully deleted'
+            })
+
+    def put(self, advertisement_id: int):
+        user_data = validation(request.json, CreateAdvertisement)
+        with Session() as session:
+            advertisement = get_object_or_404(session, Advertisement, Advertisement.id == advertisement_id)
+            token = check_token(session)
+            if token.user.id != advertisement.owner_id:
+                raise HTTPError(403, 'auth error')
+            advertisement.title = user_data['title']
+            advertisement.description = user_data['description']
+            session.commit()
+            return jsonify(advertisement.to_dict())
 
 
 def login():
-    user_data = validation(request.json, RegisterUser)
+    user_data = validation(request.json, CreateUser)
     with Session() as session:
         user = get_object_or_404(session, User, User.user_name == user_data['user_name'])
         if not user.check_password(user_data['password']):
             raise HTTPError(401, 'wrong password')
+        new_token = Token(user_id=user.id)
+        session.add(new_token)
+        session.commit()
         return jsonify({
-            'message': 'login successful'
+            'message': 'login successful',
+            'token': new_token.id,
         })
 
 
 app.add_url_rule('/user/', methods=['POST'], view_func=UserView.as_view(name='create_user'))
 app.add_url_rule('/user/<int:user_id>/', methods=['GET'], view_func=UserView.as_view(name='get_user'))
 app.add_url_rule('/login/', methods=['POST'], view_func=login)
+app.add_url_rule('/advertisement/', methods=['POST'], view_func=AdvertisementView.as_view(name='create_advertisement'))
+app.add_url_rule('/advertisement/<int:advertisement_id>/', methods=['GET'], view_func=AdvertisementView.as_view(
+    name='get_advertisement'
+))
+app.add_url_rule('/advertisement/<int:advertisement_id>/', methods=['DELETE'], view_func=AdvertisementView.as_view(
+    name='delete_advertisement'
+))
+app.add_url_rule('/advertisement/<int:advertisement_id>/', methods=['PUT'], view_func=AdvertisementView.as_view(
+    name='change_advertisement'
+))
 app.run()
---
+++ client.py
@@ -0,0 +1,126 @@
+import requests
+
+HOST = 'http://127.0.0.1:5000'
+
+print('>>> create normal user')
+data = requests.post(f'{HOST}/user/',
+                     json={
+                         'user_name': 'Will Herondale',
+                         'email': 'w.herondale@example.org',
+                         'password': 'skdskjfakjfnal1',
+                     })
+print(data.json(), '\n')
+
+print('>>> create user with too short password')
+data = requests.post(f'{HOST}/user/',
+                     json={
+                         'user_name': 'James Carstairs',
+                         'email': 'j.carstairs@example.org',
+                         'password': 'gkdn',
+                     })
+print(data.json(), '\n')
+
+print('>>> create user with redundant field')
+data = requests.post(f'{HOST}/user/',
+                     json={
+                         'user_name': 'Tessa Gray',
+                         'email': 't.gray@example.org',
+                         'password': 'dkjsnfkjbkafnk223',
+                         'blablabla': 'blablabla',
+                     })
+print(data.json(), '\n')
+
+print('>>> get user by id')
+data = requests.get(f'{HOST}/user/1/')
+print(data.json(), '\n')
+
+print('>>> login user 1')
+data = requests.post(f'{HOST}/login/',
+                     json={
+                         'user_name': 'Tessa Gray',
+                         'email': 't.gray@example.org',
+                         'password': 'dkjsnfkjbkafnk223'
+                     })
+print(data.json(), '\n')
+
+print('>>> login user 2 wrong password')
+data = requests.post(f'{HOST}/login/',
+                     json={
+                         'user_name': 'Will Herondale',
+                         'email': 'w.herondale@example.org',
+                         'password': 'skdskjfa1',
+                     })
+print(data.json(), '\n')
+
+print('>>> login user 2')
+data = requests.post(f'{HOST}/login/',
+                     json={
+                         'user_name': 'Will Herondale',
+                         'email': 'w.herondale@example.org',
+                         'password': 'skdskjfakjfnal1',
+                     })
+print(data.json(), '\n')
+
+print('>>> post advertisement without authorization')
+data = requests.post(f'{HOST}/advertisement/',
+                     json={
+                         'title': 'Куплю диван',
+                         'description': 'Рассмотрю угловые, складные и др. виды диванов',
+                     })
+print(data.json(), '\n')
+
+print('>>> post advertisement with authorization')
+data = requests.post(f'{HOST}/advertisement/',
+                     json={
+                         'title': 'Продам стол',
+                         'description': 'Дубовый стол в отличном состоянии',
+                     },
+                     headers={
+                         'user_name': 'Tessa Gray',
+                         'token': 'c0fa8f3f-6ed7-4eac-b8e4-79fb16005e69',
+                     })
+print(data.json(), '\n')
+
+print('>>> get advertisement by id')
+data = requests.get(f'{HOST}/advertisement/1/')
+print(data.json(), '\n')
+
+print('>>> change advertisement by id with the wrong user and token')
+data = requests.put(f'{HOST}/advertisement/1/',
+                    json={
+                        'title': 'Продам дубовый стол',
+                        'description': 'В отличном состоянии'
+                    },
+                    headers={
+                           'user_name': 'Will Herondale',
+                           'token': '0efa0717-8f96-4349-8425-a01c89ca5350',
+                       })
+print(data.json(), '\n')
+
+print('>>> change advertisement by id with owner token')
+data = requests.put(f'{HOST}/advertisement/1/',
+                    json={
+                        'title': 'Продам дубовый стол',
+                        'description': 'В отличном состоянии'
+                    },
+                    headers={
+                           'user_name': 'Tessa Gray',
+                           'token': 'c0fa8f3f-6ed7-4eac-b8e4-79fb16005e69',
+                       })
+print(data.json(), '\n')
+
+print('>>> delete advertisement by id with the wrong user and token')
+data = requests.delete(f'{HOST}/advertisement/1/',
+                       headers={
+                           'user_name': 'Will Herondale',
+                           'token': '0efa0717-8f96-4349-8425-a01c89ca5350',
+                       })
+print(data.json(), '\n')
+
+print('>>> delete advertisement by id with authorization')
+data = requests.delete(f'{HOST}/advertisement/1/',
+                       headers={
+                           'user_name': 'Tessa Gray',
+                           'token': 'c0fa8f3f-6ed7-4eac-b8e4-79fb16005e69',
+                       })
+print(data.json(), '\n')
